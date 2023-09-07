4e9963f2f37a5934ef65ebcbc63b824133328503
davis <davisfpatterson@gmail.com>
2023-09-07 00:27:49 -0400

    added a ton of styling

--- src/App.js
+++ src/App.js
@@ -9,43 +9,82 @@ function App() {
   return (
     // map through all the data for display, create variables for each thing
     <div>
-      <h1>Freeshelf</h1>
+      <header>
+        <h1>Freeshelf</h1>
+      </header>
       {bookData.map((book, index) => (
-        <div key={index}>
-          <Book
-            bookTitle={book.title}
-            bookAuthor={book.author}
-            bookImg={book.coverImageUrl}
-          />
-          <button
-            onClick={() => {
-              setActiveBook(index);
-              setShowMore(!showMore);
-            }}
-          >
-            More Information
-          </button>
-          {activeBook === index && showMore && (
-            <>
-              <div>URL: {book.url}</div>
-              <div>Publisher: {book.publisher}</div>
-              <div>Publication Date: {book.publicationDate}</div>
-              <div>Full Description: {book.detailedDescription}</div>
-            </>
-          )}
-        </div>
+        <>
+          <div className="container">
+            <div className="fullBox">
+              <div key={index} className="basicBox">
+                <Book
+                  bookTitle={book.title}
+                  bookAuthor={book.author}
+                  shortDesc={book.shortDescription}
+                  bookImg={book.coverImageUrl}
+                />
+              </div>
+              <div className="buttonContainer">
+                <div className="buttonBox">
+                  <button
+                    className="moreButton"
+                    onClick={() => {
+                      setActiveBook(index);
+                      setShowMore(!showMore);
+                    }}
+                  >
+                    More Information
+                  </button>
+                </div>
+              </div>
+              <div className="moreContainer">
+                <div className="moreBox">
+                  {activeBook === index && showMore && (
+                    <>
+                      <div className="bookUrl">
+                        <strong>URL:</strong> {book.url}
+                      </div>
+                      <div className="bookPub">
+                        <strong>Publisher:</strong> {book.publisher}
+                      </div>
+                      <div className="bookPubDate">
+                        <strong>Publication Date:</strong>{' '}
+                        {book.publicationDate}
+                      </div>
+                      <div className="bookFullDesc">
+                        <strong>Full Description:</strong>
+                      </div>
+                      <div className="bookFullDescText">
+                        {book.detailedDescription}
+                      </div>
+                    </>
+                  )}
+                </div>
+              </div>
+            </div>
+          </div>
+        </>
       ))}
     </div>
   );
 }
 
-const Book = ({ bookTitle, bookAuthor, bookImg }) => {
+const Book = ({ bookTitle, bookAuthor, shortDesc, bookImg }) => {
   return (
-    <div>
-      <p>{bookTitle}</p>
-      <p>{bookAuthor}</p>
-      <img src={bookImg}></img>
-    </div>
+    <>
+      <div className="infoContainer">
+        <div className="infoBox">
+          <div className="detailBox">
+            <p className="bookTitle">{bookTitle}</p>
+            <p className="bookAuthor">{bookAuthor}</p>
+            <p className="shortDesc">{shortDesc}</p>
+          </div>
+          <div className="imgBox">
+            <img src={bookImg} className="bookImg"></img>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
